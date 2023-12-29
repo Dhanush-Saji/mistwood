acd68ee3490a6328a0063fc263edcc70d634ea39
Dhanush Saji <dhanushsaji1@gmail.com>
2023-12-29 21:08:08 +0530

    29-12

--- components/Section2.jsx
+++ components/Section2.jsx
@@ -5,7 +5,6 @@ const Section2 = () => {
     <div className='flex w-screen h-screen px-10 flex-col justify-center items-end text-[#27282a]'>
       <div className='flex flex-col items-end'>
       <span className='text-[6rem] font-[800] m-0'>Dreamy spaces,</span>
-      <span className='text-[6rem] font-[800] m-0 mt-[-53px]'>real furniture.</span>
       </div>
       <p className='w-[40vw] font-medium text-right'>More than furniture, it's a feeling. Find your perfect fit</p>
     </div>
