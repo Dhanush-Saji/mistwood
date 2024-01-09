e49f5acaadcef579c2cb5fc087bb38b6452698a0
Dhanush Saji <dhanushsaji1@gmail.com>
2024-01-10 01:37:36 +0530

    10-01

--- components/Sections/Section2/Section2Content.jsx
+++ components/Sections/Section2/Section2Content.jsx
@@ -5,9 +5,10 @@ const Section2Content = () => {
   return (
     <>
     <div className='flex flex-col items-end'>
-      <p className='text-[3rem] sm:text-[6rem] font-[800] m-0 text-right text-[#27282a] dark:text-[#eeeeee]'>Dreamy spaces, real furniture.</p>
+      <p className='text-[3rem] sm:text-[6rem] font-[800] m-0 text-right text-[#27282a] dark:text-[#eeeeee]'>Elevate Home</p>
+      <p className='text-[3rem] sm:text-[6rem] font-[800] m-0 text-right text-[#27282a] dark:text-[#eeeeee]'>Comfort</p>
       </div>
-      <p className='w-full sm:w-[40vw] font-medium text-right text-[#27282a] dark:text-[#919191]'>More than furniture, it's a feeling. Find your perfect fit</p>
+      <p className='w-full sm:w-[40vw] font-medium text-right text-[#27282a] dark:text-[#919191]'>Transform your space into a haven of comfort</p>
     </>
   )
 }
