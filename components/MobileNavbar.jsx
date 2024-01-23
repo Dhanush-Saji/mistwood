79c04c0c19aa3a13ce1fd5fdf989273f71b5af25
Dhanush Saji <dhanushsaji1@gmail.com>
2024-01-24 00:23:19 +0530

    24-01

--- components/MobileNavbar.jsx
+++ components/MobileNavbar.jsx
@@ -18,13 +18,17 @@ const MobileNavbar = () => {
     </SheetHeader>
       <DarkModeToggle />
     <div className='flex flex-col items-center mt-5 gap-2'>
+    <SheetClose asChild>
     <Link href={'/'}>
         <h1 className='text-[#27282a] dark:text-[#919191] text-lg'>Profile</h1>
         </Link>
+        </SheetClose>
         <Separator className='dark:bg-[#919191]' />
+        <SheetClose asChild>
         <Link href={'/shop'}>
         <h1 className='text-[#27282a] dark:text-[#919191] text-lg'>Shop</h1>
         </Link>
+        </SheetClose>
         <Separator className='dark:bg-[#919191]' />
         <Link href={'/'}>
         <h1 className='text-[#27282a] dark:text-[#919191] text-lg'>Contact</h1>
