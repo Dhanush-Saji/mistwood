73cf0b725df1c4d118efe7c6710e2d11fd9921e9
Dhanush Saji <dhanushsaji1@gmail.com>
2024-02-02 00:10:57 +0530

    02-02

--- app/shop/[id]/page.jsx
+++ app/shop/[id]/page.jsx
@@ -1,6 +1,53 @@
 "use client";
+import LoadingCircle from "@/components/Loaders/LoadingCircle";
+import ProductSingle from "@/components/ProductSingle";
+import SliderComponent from "@/components/Slider";
+import { Button } from "@/components/ui/button";
+import { changeNumberFormat } from "@/services/Formatter";
+import { getRelatedProduct, getSingleProduct } from "@/utils/APICalls";
+import Image from "next/image";
+import React, { useState,useEffect } from "react";
 
 const Page = ({ params }) => {
+  const [qnty, setqnty] = useState(0)
+  const [product, setproduct] = useState([])
+  const [isLoading, setisLoading] = useState(false)
+  const [imageArray, setimageArray] = useState([])
+  const [relatedProduct, setrelatedProduct] = useState([])
+  const [imageIndex, setimageIndex] = useState(0)
+  const getSinglepro = async () => {
+    try {
+      const res = await getSingleProduct(params.id) || [];
+      setproduct(res)
+      let temp = []
+      if(res[0]?.product_image?.img1){
+        temp.push(res[0]?.product_image?.img1?.url)
+      }
+      if(res[0]?.product_image?.img2){
+        temp.push(res[0]?.product_image?.img2?.url)
+      }
+      if(res[0]?.product_image?.img3){
+        temp.push(res[0]?.product_image?.img3?.url)
+      }
+      setimageArray(temp)
+    } catch (error) {
+      console.error(error);
+    }
+    setisLoading(false)
+  };
+  const getRelatedPro = async () => {
+    try {
+      const res = await getRelatedProduct(params.id) || []
+      setrelatedProduct(res)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(()=>{
+    setisLoading(true)
+    getSinglepro()
+    getRelatedPro()
+  },[])
   return <div></div>
 };
 
