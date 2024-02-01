463751d9b20bb43052a898efee0ba200aaf43a54
Dhanush Saji <dhanushsaji1@gmail.com>
2024-02-02 01:17:46 +0530

    02-02

--- components/Sections/Section4/Section4.jsx
+++ components/Sections/Section4/Section4.jsx
@@ -27,27 +27,27 @@ const Section4 = async() => {
           </Link>
             ))
           } */}
-          <Link className='category1 overflow-hidden rounded-lg md:rounded-none md:rounded-tl-[4rem] max-h-[50vh] cursor-pointer relative' href={'/product-page?category=Chair'} prefetch={false}>
+          <Link className='category1 overflow-hidden rounded-lg md:rounded-none md:rounded-tl-[4rem] max-h-[50vh] cursor-pointer relative' href={'/shop?category=Chair'} prefetch={false}>
             <img src={category[0]?.category_image?.url} alt="" className='transition-all object-cover w-full h-full duration-500' />
             <div class="overlay">
               <button className='text-[#27282a] px-4 cursor-pointer py-2 rounded-full bg-white text-sm font-medium'>{category[0]?.category_name} category</button>
             </div>
           </Link>
           <div className='flex flex-col gap-3 max-h-[50vh]'>
-          <Link className='overflow-hidden rounded-lg md:rounded-none category2 cursor-pointer relative'prefetch={false} href={'/product-page?category=Bed'}>
+          <Link className='overflow-hidden rounded-lg md:rounded-none category2 cursor-pointer relative'prefetch={false} href={'/shop?category=Bed'}>
             <img src={category[1]?.category_image?.url} alt="" className='transition-all object-cover w-full h-full duration-500' />
             <div class="overlay">
               <button className='text-[#27282a] px-4 cursor-pointer py-2 rounded-full bg-white text-sm font-medium'>{category[1]?.category_name} category</button>
             </div>
             </Link>
-            <Link className=' overflow-hidden rounded-lg md:rounded-none category3 cursor-pointer relative' prefetch={false} href={'/product-page?category=Table'}>
+            <Link className=' overflow-hidden rounded-lg md:rounded-none category3 cursor-pointer relative' prefetch={false} href={'/shop?category=Table'}>
             <img src={category[2]?.category_image?.url} alt="" className='transition-all object-cover w-full h-full duration-500' />
             <div class="overlay">
               <button className='text-[#27282a] px-4 cursor-pointer py-2 rounded-full bg-white text-sm font-medium'>{category[2]?.category_name} category</button>
             </div>
             </Link>
           </div>
-          <Link className='category4 overflow-hidden rounded-lg md:rounded-none md:rounded-br-[4rem] max-h-[50vh] cursor-pointer relative' prefetch={false} href={'/product-page?category=Sofa'}>
+          <Link className='category4 overflow-hidden rounded-lg md:rounded-none md:rounded-br-[4rem] max-h-[50vh] cursor-pointer relative' prefetch={false} href={'/shop?category=Sofa'}>
           <img src={category[3]?.category_image?.url} alt="" className='transition-all object-cover w-full h-full duration-500' />
           <div class="overlay">
               <button className='text-[#27282a] px-4 cursor-pointer py-2 rounded-full bg-white text-sm font-medium'>{category[3]?.category_name} category</button>
