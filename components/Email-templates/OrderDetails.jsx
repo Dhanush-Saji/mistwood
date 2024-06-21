17b83e718a0469fb496924c75459f891136d2079
Dhanush Saji <dhanushsaji1@gmail.com>
2024-06-22 02:03:07 +0530

    22-06

--- components/Email-templates/OrderDetails.jsx
+++ components/Email-templates/OrderDetails.jsx
@@ -2,9 +2,6 @@ import { getDateddmmyyy } from '@/services/Formatter'
 import React from 'react'
 
 const OrderDetails = ({username,orderid,orderdate,products,totalAmount}) => {
-  const customOrderdate =orderdate?getDateddmmyyy(orderdate?.split('T')[0]):''
-
-
   return (
     `<html lang="en">
 <head>
@@ -34,27 +31,7 @@ const OrderDetails = ({username,orderid,orderdate,products,totalAmount}) => {
     </style>
 </head>
 <body >
-    <div style="display: flex;flex-direction: column;padding: 1rem;">
-        <h1 style="text-align: center;">Hurrayy.. Order placed!</h1>
-        <p style="opacity: 0.7;">
-            Hi ${username}, </br>
-            Thanks for placing your order with us. Below are the details of your order. If you have any queries, feel free to get in touch with us at dhanusaji1@gmail.com
-        </p>
-    <div style="display: flex; gap: 0.5rem;align-items: center;opacity: 0.7;">
-        <span style="margin: 0;">Order ID: </span>
-        <span>${orderid}</span>
-    </div>
-    <div style="display: flex; gap: 0.5rem;align-items: center;opacity: 0.7;">
-        <span style="margin: 0;">Order Date: </span>
-        <p>${customOrderdate}</p>
-    </div>
-    <div style="display: flex;flex-direction: column; gap: 0.5rem;background-color: #c6f5ff;border-radius: 10px;">
-
-    </div>
-    <p style="text-align: right;">Shipping charge: ₹0</p>
-    <div style="width: 100%;background-color: black;height: 1px;opacity: 0.2;"></div>
-    <h3 style="text-align: right;">Total: ₹${totalAmount}</h3>
-    </div>
+    fgfg
 </body>
 </html>`
   )
