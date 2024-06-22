d35e97411f3dca89c4475ed611962ffe16558f65
Dhanush Saji <dhanushsaji1@gmail.com>
2024-06-22 13:03:52 +0530

    22-06

--- components/Email-templates/OrderDetails.jsx
+++ components/Email-templates/OrderDetails.jsx
@@ -2,36 +2,9 @@ import { getDateddmmyyy } from '@/services/Formatter'
 import React from 'react'
 
 const OrderDetails = ({username,orderid,orderdate,products,totalAmount}) => {
+
   return (
-    `<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-    <title>Mail</title>
-    <style>
-        body{
-            margin: 0;
-            padding: 0;
-            font-family: 'Roboto', sans-serif;
-        }
-        .product-div{
-            display: flex;
-            align-items: center;
-            flex-direction: row;
-            padding: 1rem 2rem;
-        }
-        @media only screen and (max-width: 600px) {
-            .product-div{
-                flex-direction: column;
-            }
-        }
-    </style>
-</head>
-<body >
-    <div style="display: flex;flex-direction: column;padding: 1rem;">
+    `<div style="display: flex;flex-direction: column;padding: 1rem;">
         <h1 style="text-align: center;">Hurrayy.. Order placed!</h1>
         <p style="opacity: 0.7;">
             Hi ${username}, </br>
@@ -43,17 +16,14 @@ const OrderDetails = ({username,orderid,orderdate,products,totalAmount}) => {
     </div>
     <div style="display: flex; gap: 0.5rem;align-items: center;opacity: 0.7;">
         <span style="margin: 0;">Order Date: </span>
-  
     </div>
     <div style="display: flex;flex-direction: column; gap: 0.5rem;background-color: #c6f5ff;border-radius: 10px;">
-       
+   
     </div>
     <p style="text-align: right;">Shipping charge: ₹0</p>
     <div style="width: 100%;background-color: black;height: 1px;opacity: 0.2;"></div>
     <h3 style="text-align: right;">Total: ₹${totalAmount}</h3>
-    </div>
-</body>
-</html>`
+    </div>`
   )
 }
 
