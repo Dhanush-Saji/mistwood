37793db9005020df8dadd35c9f5d9f85b0ebbccb
Dhanush-Saji <dhanushsaji1@gmail.com>
2024-06-26 14:15:16 +0530

    26-06

--- middleware.js
+++ middleware.js
@@ -1,5 +1,15 @@
 import { NextResponse } from "next/server";
 
 export default async function middleware(req) {
+  const { pathname } = req.nextUrl;
+
+  if (pathname === "/shop") {
+    return NextResponse.next();
+  }
+  const isProtectedRoute = pathname.startsWith("/cart") || pathname.startsWith("/shop/");
+  if ( isProtectedRoute) {
+    return NextResponse.redirect(new URL("/login", req.url));
+  }
+
   return NextResponse.next();
 }
