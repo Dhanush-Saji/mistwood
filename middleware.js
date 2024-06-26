ec5635d984cb18398e9e42847313f5135d24bc46
Dhanush-Saji <dhanushsaji1@gmail.com>
2024-06-26 16:23:19 +0530

    26-06

--- middleware.js
+++ middleware.js
@@ -1,3 +1,18 @@
 export { default } from "next-auth/middleware"
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from 'next/server'
+ 
 
-export const config = { matcher: ["/cart","/orders"] }
+export async function middleware(request) {
+  const token = await getToken({req:request})
+  const url = request.nextUrl
+  if(!token && (url.pathname.startsWith('/cart') || url.pathname.startsWith('/orders'))){
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
+  return NextResponse.next();
+}
+ 
+
+export const config = {
+  matcher: ['/cart','/orders'],
+}
