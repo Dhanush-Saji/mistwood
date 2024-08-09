a697258ac98628cb9cf54bb2c0aa5ef47fe765ca
Dhanush-Saji <dhanushsaji1@gmail.com>
2024-08-09 12:39:01 +0530

    09-08

--- components/Sections/Section2/Section2Wrapper.jsx
+++ components/Sections/Section2/Section2Wrapper.jsx
@@ -1,26 +1,48 @@
 "use client"
 import React, { useLayoutEffect } from 'react'
-import {gsap} from 'gsap';
+import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
+import GridPattern from '@/components/magicui/grid-pattern';
+import { cn } from '@/lib/utils';
 gsap.registerPlugin(ScrollTrigger)
 
-const Section2Wrapper = ({children}) => {
-    useLayoutEffect(()=>{
-        let secondElem = ScrollTrigger.create({
-          trigger: '.second-section',
-          start: "top top",
-          scrub: 3,
-          pin:true,
-          pinSpacing:false
-        });
-        return()=>{
-          if(secondElem) secondElem.kill()
-        }
-        },[])
+const Section2Wrapper = ({ children }) => {
+  useLayoutEffect(() => {
+    let secondElem = ScrollTrigger.create({
+      trigger: '.second-section',
+      start: "top top",
+      scrub: 3,
+      pin: true,
+      pinSpacing: false
+    });
+    return () => {
+      if (secondElem) secondElem.kill()
+    }
+  }, [])
   return (
     <div className=' second-section relative flex w-screen h-screen px-7 sm:px-10 flex-col pt-[10vh] justify-center items-end text-[#252525] bg-white dark:bg-neutral-700'>
-{children}
-  </div>
+      <GridPattern
+        squares={[
+          [4, 4],
+          [5, 1],
+          [8, 2],
+          [5, 3],
+          [5, 5],
+          [10, 10],
+          [12, 15],
+          [15, 10],
+          [10, 15],
+          [15, 10],
+          [10, 15],
+          [15, 10],
+        ]}
+        className={cn(
+          "[mask-image:radial-gradient(400px_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
+        )}
+      />
+      {children}
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,9 @@
+// const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
@@ -77,5 +83,16 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate"),require('@tailwindcss/forms')],
+  plugins: [require("tailwindcss-animate"),require('@tailwindcss/forms'),addVariablesForColors,],
+}
+
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
 }
