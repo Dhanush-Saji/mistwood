f78b4516821d64f96c20498a6aad9462df0382c7
Dhanush-Saji <dhanushsaji1@gmail.com>
2024-08-17 17:36:49 +0530

    17-08

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -10,15 +10,15 @@ export async function GET(req){
         const query = req.nextUrl.searchParams.get('category')
         const categories = await CategoryModel.find({})
         const reqCategory = categories.find((category)=>category.category_name == query)
-        const products = await ProductModel.find({...((query != null && query != 'All' && query != 'null')?{category:reqCategory?._id}:{})})
+        const products = await ProductModel.find({...((query != null && query != 'All' && query != 'null')?{category:reqCategory?._id}:{}),sellingprice: { $exists: true, $ne: null }})
         .select('-__v').populate('discounts').lean().exec()
-        // products?.map((product,index)=>{
-        //     if(product?.price){
-        //       let tax = (product?.price*product?.gst*0.01)-(product?.purchase_cost*product?.gst*0.01)
-        //       product.tax_payable = tax
-        //       product.sellingprice = product?.price * ((product?.gst+100)/100)
-        //   }
-        //    })
+        products?.map((product,index)=>{
+            if(product?.price){
+              let tax = (product?.price*product?.gst*0.01)-(product?.purchase_cost*product?.gst*0.01)
+              product.tax_payable = tax
+              product.sellingprice = product?.price * ((product?.gst+100)/100)
+          }
+           })
         return NextResponse.json(products,{status:200})
     } catch (error) {
         return NextResponse.json({error},{status:500})
