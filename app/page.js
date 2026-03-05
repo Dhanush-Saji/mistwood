e970e8941392d49c23a11cc48cfc40607267c349
Dhanush-Saji <dhanushsaji1@gmail.com>
2026-03-05 15:48:24 +0530

    05-03

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import Section1 from "@/components/Sections/Section1/Section1";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <>
    <Header />
+   <Section1 />
    </>
   );
 }
