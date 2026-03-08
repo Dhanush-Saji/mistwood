4db5fa2fc03d3f259559dc4df6a3685fb35a8d39
Dhanush-Saji <dhanushsaji1@gmail.com>
2026-03-08 19:26:59 +0530

    08-03

--- components/CategoryListCount.jsx
+++ components/CategoryListCount.jsx
@@ -1,12 +1,25 @@
 import Link from 'next/link'
 import React from 'react'
+import CategoryCountBtn from './Button/CategoryCountBtn'
 import { getCommonApi } from '@/utils/APICalls'
-const url = process.env.BACKEND_URL
 
 const CategoryListCount = async() => {
   const countList = await getCommonApi('/api/countCategory')
   return (
     <>
+    {
+      countList?.length>0?
+      <>
+      <div className="grid grid-cols-5 gap-[0.5rem] w-full justify-center mt-[0.7rem]">
+      {
+          countList?.length>0 && countList?.map((category,index)=>(
+            <CategoryCountBtn key={index} index={index} category={category} />
+          ))
+        }
+      </div>
+      </>:
+      <></>
+    }
       </>
   )
 }
