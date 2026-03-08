f0f6fef9f838db5d82fd012116476a9c240d30d5
Dhanush-Saji <dhanushsaji1@gmail.com>
2026-03-08 19:29:09 +0530

    08-03

--- components/CategoryListCount.jsx
+++ components/CategoryListCount.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import LoadingCircle from './Loaders/LoadingCircle'
 import CategoryCountBtn from './Button/CategoryCountBtn'
 import { getCommonApi } from '@/utils/APICalls'
 
@@ -13,12 +14,14 @@ const CategoryListCount = async() => {
       <div className="grid grid-cols-5 gap-[0.5rem] w-full justify-center mt-[0.7rem]">
       {
           countList?.length>0 && countList?.map((category,index)=>(
-            <CategoryCountBtn key={index} index={index} category={category} />
+            <div key={index}></div>
           ))
         }
       </div>
       </>:
-      <></>
+      <div className='w-full flex justify-center  mt-[2rem]'>
+      <LoadingCircle />
+      </div>
     }
       </>
   )
