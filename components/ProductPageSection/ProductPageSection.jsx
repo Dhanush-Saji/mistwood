00b4ac47a978372c7353c3e4bc3585cad08ff0aa
Dhanush-Saji <dhanushsaji1@gmail.com>
2026-03-08 19:46:21 +0530

    08-03

--- components/ProductPageSection/ProductPageSection.jsx
+++ components/ProductPageSection/ProductPageSection.jsx
@@ -1,47 +1,47 @@
 'use client'
-import { getProduct } from '@/utils/APICalls';
+import { getCommonApi } from '@/utils/APICalls';
 import dynamic from 'next/dynamic';
 import { useSearchParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
+import React, { Suspense, useEffect, useState } from 'react'
 import LoadingCircle from '../Loaders/LoadingCircle';
 import ProductSingle from '../ProductSingle';
 const NoData = dynamic(() => import("@/components/Loaders/NoData"), {
-    ssr: false,
-  });
+  ssr: false,
+});
 
 const ProductPageSection = () => {
-    const searchParams = useSearchParams()
-    const search = searchParams.get('category')
-    const [productData, setproductData] = useState([]);
-    const [isLoading, setisLoading] = useState(false)
-    
-    const getPro = async () => {
-      setproductData([])
-      setisLoading(true)
-      try {
-        const res = await getProduct(search);
-        setproductData(res);
-      } catch (error) {
-        console.log(error);
-      }
-      setisLoading(false)
-    };
-    useEffect(() => {
-      getPro();
-    }, [search]);
+  const searchParams = useSearchParams()
+  const search = searchParams.get('category')
+  const [productData, setproductData] = useState([]);
+  const [isLoading, setisLoading] = useState(false)
+
+  const getPro = async () => {
+    setproductData([])
+    setisLoading(true)
+    try {
+      const res = await getCommonApi(`/api/products?category=${search}`);
+      setproductData(res);
+    } catch (error) {
+      console.log(error);
+    }
+    setisLoading(false)
+  };
+  useEffect(() => {
+    getPro();
+  }, [search]);
   return (
-   <>
-   {isLoading?<div className='w-full flex justify-center  mt-[2rem]'>
-    <LoadingCircle />
-    </div>:productData?.length == 0? <NoData />:null}
-    <div className="product-grid-list mt-5 gap-4">
-    {productData?.length>0 && productData?.map((product, index) => (
-      <React.Fragment key={index}>
-      <ProductSingle product={product} />
-      </React.Fragment>
-    ))}
-    </div>
-   </>
+    <>
+      {isLoading ? <div className='w-full flex justify-center  mt-[2rem]'>
+        <LoadingCircle />
+      </div> : productData?.length == 0 ? <NoData /> : null}
+      <div className="product-grid-list mt-5 gap-4">
+        <Suspense fallback={<LoadingCircle />}>
+          {productData?.length > 0 && productData?.map((product, index) => (
+            <ProductSingle key={index} product={product} />
+          ))}
+        </Suspense>
+      </div>
+    </>
   )
 }
 
