6fcd5bd2803b3668ffa1786c0cf0afdf6a9f8a35
Dhanush-Saji <dhanushsaji1@gmail.com>
2026-03-08 19:11:24 +0530

    08-03

--- app/(with-header)/shop/page.jsx
+++ app/(with-header)/shop/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import ProductBanner from '@/public/images/product-page-banner.png'
+import CategoryListCount from "@/components/CategoryListCount";
 
 
 const Page = () => {
@@ -10,7 +11,7 @@ const Page = () => {
     <div>
       <Image width={'150px'} height={400} className='w-full rounded-[8px]' src={ProductBanner} alt="product-banner" />
     </div>
-   
+    <CategoryListCount customParams={customParams} />
   </div>
   );
 };
