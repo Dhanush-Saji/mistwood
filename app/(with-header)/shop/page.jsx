35df52e2bc6a793346a89a4612467837d11b5e56
Dhanush-Saji <dhanushsaji1@gmail.com>
2026-03-08 19:22:56 +0530

    08-03

--- app/(with-header)/shop/page.jsx
+++ app/(with-header)/shop/page.jsx
@@ -1,17 +1,14 @@
 import Image from "next/image";
 import React from "react";
 import ProductBanner from '@/public/images/product-page-banner.png'
-import CategoryListCount from "@/components/CategoryListCount";
 
 
 const Page = () => {
-  let customParams = ''
   return (
     <div className="flex-col items-center bg-white dark:bg-neutral-800 w-screen min-h-screen relative pb-6 px-4 sm:px-[2rem] pt-[12vh] sm:pt-[12vh]">
     <div>
       <Image width={'150px'} height={400} className='w-full rounded-[8px]' src={ProductBanner} alt="product-banner" />
     </div>
-    <CategoryListCount />
   </div>
   );
 };
